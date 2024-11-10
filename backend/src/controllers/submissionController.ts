a990d9ca12355f8050d0f05126d3128db413c667
Pawan <pandeypawan2061@gmail.com>
2024-11-10 22:53:08 +0530

    feat: working on most languages

--- backend/src/controllers/submissionController.ts
+++ backend/src/controllers/submissionController.ts
@@ -25,11 +25,6 @@ export const createSubmission = async (
       });
     }
 
-    // const input: Redispayload = {
-    //   code: code,
-    //   languageId: languageId,
-    //   problemId: problemId,
-    // };
     const input = {
       language_id: parseInt(languageId),
       source_code: btoa(code),
@@ -37,33 +32,7 @@ export const createSubmission = async (
 
     const resp = await createSubmissionCode(input);
 
-    // const showResult = async () => {
-    //   const result: any = await getSubmission(resp.token);
-    //   console.log(result, "result is here");
-
-    //   if (result.status_id === 1 || result.status_id === 2) {
-    //     console.log("inside");
-
-    //     await new Promise((resolve) => setTimeout(resolve, 4000));
-    //   } else {
-    //     const decodedOutput = result?.stdout ? atob(result.stdout) : "";
-    //     return decodedOutput;
-    //   }
-    // };
-
     const finalOut = await getResults(resp.token);
-
-    // await redisClient.lPush(requestQueue, JSON.stringify(input));
-
-    // await redisClient.lPush(requestQueue, JSON.stringify(code));
-
-    // const output = await redisClient.brPop(responseQueue, 0);
-
-    // const ans = await runCode(code);
-    // console.log("popping the requeust");
-
-    // const output = await redisClient.brPop(responseQueue, 0);
-    // console.log("popped the response", output);
 
     return res.status(200).json({
       answer: JSON.parse(JSON.stringify(finalOut)),
@@ -74,3 +43,15 @@ export const createSubmission = async (
     });
   }
 };
+
+// await redisClient.lPush(requestQueue, JSON.stringify(input));
+
+// await redisClient.lPush(requestQueue, JSON.stringify(code));
+
+// const output = await redisClient.brPop(responseQueue, 0);
+
+// const ans = await runCode(code);
+// console.log("popping the requeust");
+
+// const output = await redisClient.brPop(responseQueue, 0);
+// console.log("popped the response", output);
